c02d16eb6f07c95ceb685f8b78fa65686019ee86
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-15 14:53:14 +0500

    header added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-view></router-view>
+  <div class="container">
+    <div class="search_wrapper"></div>
+    <div class="map_wrapper"></div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
