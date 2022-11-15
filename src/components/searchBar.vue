5596e8c900982aef5adaa5644dd9b20c543609a0
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-15 15:28:15 +0500

    header is completed

---
+++ src/components/searchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search_wrapper">
+    <input
+      placeholder="Search for any IP address or domain"
+      type="text"
+      name="ip-tracker-search"
+      id="ip-tracker-search"
+    />
+    <div class="icon_wrapper">
+      <icon_arrowVue></icon_arrowVue>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import icon_arrowVue from "./icons/icon_arrow.vue";
+
+export default defineComponent({
+  components: { icon_arrowVue },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="container">
-    <div class="search_wrapper"></div>
-    <div class="map_wrapper"></div>
+    <div class="search_container">
+      <h1 class="title">IP Address Tracker</h1>
+      <Search></Search>
+    </div>
+    <div class="map_container"></div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import Search from "@/components/searchBar.vue";
 
 export default defineComponent({
+  components: { Search },
   setup() {
     return {};
   },
