e53b2e3854c8c6615b7d9a6bcb6a44f5b359b213
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-17 00:25:44 +0500

    ipinfo comp completed

--- src/components/IpInfo.vue
+++ src/components/IpInfo.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="ipInfo"></div>
+  <div class="ipInfo">
+    <div class="section">
+      <p>IP ADDRESS</p>
+      <h2>192.212.174.101</h2>
+    </div>
+    <hr />
+    <div class="section">
+      <p>LOCATION</p>
+      <h2>Brooklyen, NY 1001</h2>
+    </div>
+    <hr />
+    <div class="section">
+      <p>TIMEZONE</p>
+      <h2>UTC -50:00</h2>
+    </div>
+    <hr />
+    <div class="section">
+      <p>ISP</p>
+      <h2>SpaceX StarLink</h2>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
