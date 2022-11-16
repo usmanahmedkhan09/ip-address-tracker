7ff5f294bd29d9191d44aaec0fea6e0d1e81dac6
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-16 14:00:02 +0500

    move all logic to composable

--- src/components/map.vue
+++ src/components/map.vue
@@ -3,33 +3,12 @@
 </template>
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import leaflet from "leaflet";
+
 import { useIpAddressTracker } from "@/composable/ip-address-tracker";
 export default defineComponent({
   components: {},
   setup() {
-    let mymap;
-
     const {} = useIpAddressTracker();
-
-    onMounted(() => {
-      mymap = leaflet.map("mapid").setView([51.505, -0.09], 13);
-      leaflet
-        .tileLayer(
-          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidXNtYW5haG1lZGtoYW4wOSIsImEiOiJjbGFqY3owOGMwODE5M3dud2QzZGR4dGV2In0.O_7M3lX_ZvcjaEBDkwx9KQ",
-          {
-            attribution:
-              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: "mapbox/streets-v11",
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken:
-              "pk.eyJ1IjoidXNtYW5haG1lZGtoYW4wOSIsImEiOiJjbGFqY3owOGMwODE5M3dud2QzZGR4dGV2In0.O_7M3lX_ZvcjaEBDkwx9KQ",
-          }
-        )
-        .addTo(mymap);
-    });
 
     return {};
   },
--- src/composable/ip-address-tracker.ts
+++ src/composable/ip-address-tracker.ts
@@ -1,12 +1,16 @@
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from "axios";
+import leaflet from "leaflet";
 
 const ipAddress = ref('')
 
 const ipAddressInfo = ref<any>(null)
 
+
 export const useIpAddressTracker = () =>
 {
+    let mymap: any;
+
     const getIpAddressInfo = async () =>
     {
         try
@@ -23,15 +27,36 @@ export const useIpAddressTracker = () =>
                 lat: result.location.lat,
                 lng: result.location.lng,
             };
-            // leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
-            // mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
-        } catch (err)
+            leaflet.marker([ipAddressInfo.value.lat, ipAddressInfo.value.lng]).addTo(mymap);
+            mymap.setView([ipAddressInfo.value.lat, ipAddressInfo.value.lng], 13);
+        } catch (err: any)
         {
-            // alert(err.message);
+            alert(err.message);
         }
     };
 
+    onMounted(() =>
+    {
+        mymap = leaflet.map("mapid").setView([51.505, -0.09], 13);
+        leaflet
+            .tileLayer(
+                "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidXNtYW5haG1lZGtoYW4wOSIsImEiOiJjbGFqY3owOGMwODE5M3dud2QzZGR4dGV2In0.O_7M3lX_ZvcjaEBDkwx9KQ",
+                {
+                    attribution:
+                        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: "mapbox/streets-v11",
+                    tileSize: 512,
+                    zoomOffset: -1,
+                    accessToken:
+                        "pk.eyJ1IjoidXNtYW5haG1lZGtoYW4wOSIsImEiOiJjbGFqY3owOGMwODE5M3dud2QzZGR4dGV2In0.O_7M3lX_ZvcjaEBDkwx9KQ",
+                }
+            )
+            .addTo(mymap);
+    });
+
     return {
+        mymap,
         ipAddress,
         ipAddressInfo,
         getIpAddressInfo
